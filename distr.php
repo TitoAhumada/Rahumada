d965cc7bedf169ab24e097b8f86beb22cb0c2b6c
TitoAhumada <68362904+TitoAhumada@users.noreply.github.com>
2024-01-26 00:46:42 -0300

    Add files via upload

--- distr.php
+++ distr.php
@@ -1,25 +1,95 @@
 <html>
     <head>
         <title>Distribución de Tránsito</title>
+        <link rel="stylesheet" href="styles/stylesheet.css">
     </head>
 
     <body>
         <h1>Distribución de fechas de Licencias</h1>
 
         <form method="POST" action="distr_calc.php">
-            <h3>Tipo de Licencia:</h3>
-            <select id="tipo_lic" name="licencia">
-                <option value="profesional">Profesional</option>
-                <option value="no_profesional">No Profesional</option>
+
+        <h3>Fecha de Nacimiento:</h3>
+        <input type="date" name="f_nacimiento" />
+
+            <h3>Licencias Solicitadas:</h3>
+            <h4>Profesional: </h4>
+                <input type="checkbox" id="a1_19495" name="a1_19495" value="a1">
+                <label for="a1_19495"> A 1</label>
+                <input type="checkbox" id="a2_19495" name="a2_19495" value="a2">
+                <label for="a2_19495"> A 2</label>
+                <input type="checkbox" id="a3_19495" name="a3_19495" value="a3">
+                <label for="a3_19495"> A 3</label>
+                <input type="checkbox" id="a4_19495" name="a4_19495" value="a4">
+                <label for="a2_19495"> A 4</label>
+                <input type="checkbox" id="a5_19495" name="a5_19495" value="a5">
+                <label for="a3_19495"> A 5</label><br>
+                <input type="checkbox" id="a1_18290" name="a1_18290" value="a1_18290">
+                <label for="a1_18290"> A 1 Ley 18.290 </label>
+                <input type="checkbox" id="a2_18290" name="a2_18290" value="a2_18290">
+                <label for="a2_18290"> A 2 Ley 18.290 </label><br>
+
+            <h4>No profesional: </h4>
+                <input type="checkbox" id="b_18290" name="b_18290" value="b">
+                <label for="b_18290"> B </label>
+                <input type="checkbox" id="c_18290" name="c_18290" value="c">
+                <label for="b_18290"> C </label>
+
+            <h4>Especiales: </h4>
+                <input type="checkbox" id="d_18290" name="d_18290" value="d">
+                <label for="d_18290"> D </label>
+                <input type="checkbox" id="e_18290" name="e_18290" value="e">
+                <label for="e_18290"> E </label>
+                <input type="checkbox" id="f_18290" name="f_18290" value="f">
+                <label for="f_18290"> F </label>
+
+            
+            <h3>Examen Médico: </h3>
+            <h4>Examinador:
+            <select id="ev_medica" name="ev_medica">
+                <option value="x"> </option>
+                <option value="c_lopez">Crescente López</option>
+                <option value="m_manzanilla">Maria Manzanilla</option>
+                <option value="r_matamala">Ricardo Matamala</option>
+                <option value="p_jara">Paula Jara</option>
             </select>
 
-            <h3>Control Restringido: </h3>
+            Fecha de Examen Médico: 
+            <input type="date" name="f_examen" /> </h4>
+
+            <h3>Examen Teórico: </h3>
+            <h4>Examinador: 
+            <select id="ex_teorico" name="ex_teorico">
+                <option value=" "> </option>
+                <option value="r_alvarez">Ronald Álvarez</option>
+                <option value="l_esquivel">Luciano Esquivel</option>
+                <option value="c_orellana">Cristian Orellana</option>
+                <option value="a_tapia">Alex Tapia</option>
+            </select>
+
+            Fecha de Examen Teórico: 
+            <input type="date" name="f_examen_teorico"/> </h4>
+
+            <h3>Examen Práctico: </h3>
+            <h4>Examinador: 
+            <select id="ex_practico" name="ex_practico">
+                <option value="x"> </option>
+                <option value="r_alvarez">Ronald Álvarez</option>
+                <option value="l_esquivel">Luciano Esquivel</option>
+                <option value="c_orellana">Cristian Orellana</option>
+                <option value="a_tapia">Alex Tapia</option>
+            </select>
+
+            Fecha de Examen Práctico: 
+            <input type="date" name="f_examen_practico" /> </h4>
+
+            <h3>Control Restringido:
             <select id="restriccion" name="restriccion">
                 <option value="no">No</option>
                 <option value="si">Si</option>
             </select>
 
-            <h3>Años de Control Restringido (Médico o Antecedente): </h3>
+            Años de Control Restringido (Médico o Antecedente): 
             <select id="a_c_restr" name="a_c_restr">
                 <option value="0">Sin Restricción</option>
                 <option value="0,5">6 Meses</option>
@@ -28,16 +98,10 @@
                 <option value="3">3 Años</option>
                 <option value="4">4 Años</option>
                 <option value="5">5 Años</option>
-            </select>
-
-            <h3>Fecha de Nacimiento:</h3>
-            <input type="date" name="f_nacimiento" />
+            </select> </h3>
 
             <h3>Fecha de Ultimo Control: </h3>
             <input type="date" name="f_ultimocontrol" />
-
-            <h3>Fecha de Examen Médico: </h3>
-            <input type="date" name="f_examen" />
 
             <input type="submit">
 
--- distr_calc.php
+++ distr_calc.php
@@ -1,63 +1,163 @@
 <?php
+    $a1 = isset($_POST['a1_19495']);
+    $a2 = isset($_POST['a2_19495']);
+    $a3 = isset($_POST['a3_19495']);
+    $a4 = isset($_POST['a4_19495']);
+    $a5 = isset($_POST['a5_19495']);
+    $a1_18290 = isset($_POST['a1_18290']);
+    $a2_18290 = isset($_POST['a2_18290']);
+    $b = isset($_POST['b_18290']);
+    $c = isset($_POST['c_18290']);
+    $d = isset($_POST['d_18290']);
+    $e = isset($_POST['e_18290']);
+    $f = isset($_POST['f_18290']);
 
-    $licencia = $_POST['licencia'];
+    if ($a1 || $a2 || $a3 || $a4 || $a5 || $a1_18290 || $a2_18290){
+        $licencia = 'profesional';
+    }
+    else{
+        $licencia = 'no_profesional';
+    }
+
+    echo "Licencias solicitadas: <br>";
+    if ($a1){
+        echo "A1 / ";
+    }
+    if ($a2){
+        echo "A2 /";
+    }
+    if ($a3){
+        echo "A3 /";
+    }
+    if ($a4){
+        echo "A4 /";
+    }
+    if ($a5){
+        echo "A5";
+    }
+
+    if ($a1 || $a2 || $a3 || $a4 || $a5){
+        echo " Ley 19.495 / ";
+    }
+
+    if ($a1_18290 || $a2_18290){
+            if ($a1_18290){
+                echo "A1";
+            }
+            if ($a1_18290 && $a2_18290){
+                echo " / ";
+            }
+            if ($a2_18290){
+                echo "A2";
+            }
+            echo " Ley 18.290";
+    }
+
+    if($b){
+        echo "B / ";
+    }
+    if($c){
+        echo "C / ";
+    }
+    if($d){
+        echo "D / ";
+    }
+    if($e){
+        echo "E / ";
+    }
+    if($f){
+        echo "F / ";
+    }
+    echo "<br>";
+    
     echo "Tipo de Licencia: ".$licencia. "<br>";
     $restriccion = $_POST['restriccion'];
     echo "Posee restricción: ".$restriccion. "<br>";
     $fecha_medico = $_POST['f_examen'];
-    echo "Realizó examen médico el: ".$fecha_medico. "<br>";
+    echo "Realizó examen médico el: ".$fecha_medico. "<tab>";
+    $fecha_teorico = $_POST['f_examen_teorico'];
+    echo " Examinador: " .$_POST['ev_medica']. "  <br>";
+    echo "Realizó examen teórico el: ".$fecha_teorico. "<tab>";
+    $fecha_practico = $_POST['f_examen_practico'];
+    echo " Examinador: " .$_POST['ex_teorico']. " <br>";
+    echo "Realizó examen práctico el: ".$fecha_practico. "<tab>";
     $fecha_nacimiento = $_POST['f_nacimiento'];
-    echo "Fecha de nacimiento: ".$fecha_nacimiento. "<br>";
+    echo " Examinador: " .$_POST['ex_practico']. "<br>";
+    $fecha_ult_control = $_POST['f_ultimocontrol'];
 
+    echo "Fecha de nacimiento: ".$fecha_nacimiento. "<br>";
+    echo "Fecha de control anterior: ".$fecha_ult_control. "<br>";
+
+    $f_medico = strtotime($fecha_medico);
+    $f_teorico = strtotime($fecha_teorico);
+    $f_practico = strtotime($fecha_practico);
+    $f_u_control = strtotime($fecha_ult_control);
+
+    if($f_practico > $f_teorico){
+        $f_control = $f_practico;
+    }
+    else if($f_teorico > $f_medico){
+        $f_control = $f_teorico;
+    }
+    else{
+        $f_control = $f_medico;
+    }
+
+    
     if ($restriccion == 'si'){
         $anos_restriccion = $_POST['a_c_restr'];
+        $ano_control = date("Y", $f_control);
+        $mes_control = date("m", $f_control);
+        $dia_control = date("d", $f_control);
         if($anos_restriccion == '0,5'){
-            $mod_date = strtotime($fecha_medico."+ 6 months");
+            $mod_date = $f_control."+ 6 months";
             echo date("d-m-Y",$mod_date). "<br>";
         }  
         else if($anos_restriccion == '1'){
-            $mod_date = strtotime($fecha_medico."+ 1 years");
-            echo date("d-m-Y",$mod_date). "<br>";
+            $ano_control = $ano_control + 1;
         }  
         else if($anos_restriccion == '2'){
-            $mod_date = strtotime($fecha_medico."+ 2 years");
-            echo date("d-m-Y",$mod_date). "<br>";
+            $ano_control = $ano_control + 2;
         }  
         else if($anos_restriccion == '3'){
-            $mod_date = strtotime($fecha_medico."+ 3 years");
-            echo date("d-m-Y",$mod_date). "<br>";
+            $ano_control = $ano_control + 3;
         }  
         else if($anos_restriccion == '4'){
-            $mod_date = strtotime($fecha_medico."+ 4 years");
-            echo date("d-m-Y",$mod_date). "<br>";
+            $ano_control = $ano_control + 4;
         }  
         else if($anos_restriccion == '5'){
-            $mod_date = strtotime($fecha_medico."+ 5 years");
-            echo date("d-m-Y",$mod_date)."<br>";
-        }   
+            $ano_control = $ano_control + 5;
+        }
+        echo "Fecha próximo examen: ".$dia_control."-".$mes_control."-".$ano_control."<br>";
+    
     }
     else if ($restriccion == 'no'){
-        $f_medico = strtotime($fecha_medico);
-        $ano_medico = date("Y", $f_medico);
-        $mes_medico = date("m", $f_medico);
-        $dia_medico = date("d", $f_medico);
+        $fecha_control = $f_control;
+        $a_ult_control = date("Y", $f_u_control);
+        $ano_control = date("Y", $fecha_control);
+        $mes_control = date("m", $fecha_control);
+        $dia_control = date("d", $fecha_control);
+
+        $f_nacimiento = strtotime($fecha_nacimiento);
+        $mes_nacimiento = date("m", $f_nacimiento);
+        $dia_nacimiento = date("d", $f_nacimiento);
+
         if($licencia == 'profesional'){
-            $ano_control = $ano_medico + 4;
-            $f_nacimiento = strtotime($fecha_nacimiento);
-            $mes_nacimiento = date("m", $f_nacimiento);
-            $dia_nacimiento = date("d", $f_nacimiento);
-            echo "Fecha próximo examen: ".$dia_nacimiento."-".$mes_nacimiento."-".$ano_control."<br>";
-            
+            $ano_control = $ano_control + 4;
         }
         else if($licencia == 'no_profesional'){
-            $ano_control = $ano_medico + 6;
-            $f_nacimiento = strtotime($fecha_nacimiento);
-            $mes_nacimiento = date("m", $f_nacimiento);
-            $dia_nacimiento = date("d", $f_nacimiento);
-            echo "Fecha próximo examen: ".$dia_nacimiento."-".$mes_nacimiento."-".$ano_control."<br>";
+            if($a_ult_control  >= 2020 && $a_ult_control <= 2023){
+                $ano_control = $a_ult_control + 6;
+            }else{
+                $ano_control = $ano_control + 6;
+            } 
         }
-
+        echo "Fecha próximo examen: ".$dia_nacimiento."-".$mes_nacimiento."-".$ano_control."<br>";
     }
+
+   $f_medico = strtotime($fecha_medico);
+
+
 
     echo "<br><br>Copyright 2024, Roberto Ahumada R. ";
 
