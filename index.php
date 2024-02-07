4a3d06ace119404878d273c201d9e3296d6d06c1
TitoAhumada <68362904+TitoAhumada@users.noreply.github.com>
2024-02-07 19:41:14 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
   <h1>Veamos si funciona</h1>
   <h3>Dpto. de Tránsito</h3>
     <a href="Rahumada/transito/distr.php">Guía Distribución de Licencias de Conducir</a>
+    <br>
 
     <a href="Rahumada/transito/infracciones.php">Generador de Texto Infracciones</a>
+    <br>
 
     <a href="Rahumada/transito/rut_grande.php">Rut grande para Fotografía</a>
+    <br>
 
-    <a href="Rahumada/transito/agenda_interna.php">Agenda Interna de Horass</a>
+    <a href="Rahumada/transito/agenda_interna.php">Agenda Interna de Horas</a>
+    <br>
 
 
 
