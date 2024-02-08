fdfcf7b6ff4dea8c34051ac881fc08610c2835c1
TitoAhumada <68362904+TitoAhumada@users.noreply.github.com>
2024-02-07 23:41:59 -0300

    Add files via upload

---
+++ transito/rut_grande.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <form method="POST" action="mostrar_rut.php">
+            Ingrese rut:
+            <input type="text" size=50 style="height:300px" name="rut" class="rut" required placeholder="19995222-K">
+            <button type="submit">Enviar</button>
+        </form>
+        
+    </body>
+</html>
