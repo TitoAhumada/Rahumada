0c7b3321025d7d207cd0a0aa3e19336006c94896
Roberto Ahumada Rojas <r.ahumadarojas@uandresbello.edu>
2024-02-19 00:03:13 -0300

    modifica index

--- transito/index.php
+++ transito/index.php
@@ -6,7 +6,7 @@
   <h1>Veamos si funciona</h1>
   <h3>Dpto. de Tránsito</h3>
 
-  
+    <!---
     <a href="Rahumada/transito/distr.php">Guía Distribución de Licencias de Conducir</a><br>
     <a href="distr.php">Guía Distribución de Licencias de Conducir - local</a><br>
     <br>
@@ -16,7 +16,9 @@
     <a href="Rahumada/transito/rut_grande.php">Rut grande para Fotografía</a><br>
     <a href="rut_grande.php">Rut grande para Fotografía - local</a><br>
     <br>
-    <a href="Rahumada/transito/agenda/agenda_web.php">Agenda Interna de Horas</a><br>
+    -->
+
+    <a href="Rahumada/transito/principal.php">DTTP - Ovalle</a><br>
     <a href="agenda/agenda_web.php">Agenda Interna de Horas - local</a><br>
 
 </body>
